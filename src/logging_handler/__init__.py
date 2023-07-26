5a51a408aaba65f97c10b113aa98b21640506fbd
Thomas Dunteman <104242297+LearningToPi@users.noreply.github.com>
2023-07-26 14:16:17 -0700

    v1.0.7 global vars now strings

--- src/logging_handler/__init__.py
+++ src/logging_handler/__init__.py
@@ -28,31 +28,30 @@ from glob import glob
 from datetime import datetime, timedelta
 import pathlib
 import os
-from functools import singledispatch
 
-__VERSION__ = (1, 0, 2)
+__VERSION__ = (1, 0, 7)
 
-DEBUG = logging.DEBUG
-INFO = logging.INFO
-WARNING = logging.WARNING
-ERROR = logging.ERROR
-CRITICAL = logging.CRITICAL
+DEFAULT_LEVEL = logging.WARNING
 
-_log_level_name = {
-    'DEBUG': DEBUG,
-    "INFO": INFO,
-    "WARNING": WARNING,
-    "ERROR": ERROR,
-    "CRITICAL": CRITICAL
+DEBUG = 'DEBUG'
+INFO = 'INFO'
+WARNING = 'WARNING'
+ERROR = 'ERROR'
+CRITICAL = 'CRITICAL'
+
+_log_level_number = {
+    'DEBUG': logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL
 }
-
-_supported_log_levels = [DEBUG, INFO, WARNING, ERROR, CRITICAL]
 
 
 def create_logger(console_level=WARNING, log_file='', file_level=WARNING, name='', file_mode='a', console=True,
-                  syslog=False, syslog_script_name='', log_file_vars=[], log_file_retention_days=0, propagate=False):
+                  syslog=False, syslog_script_name='', log_file_vars=None, log_file_retention_days=0, propagate=False):
     """ Creates a logger and returns the handle.
-        Log file vars should be sent as a dict -> {"var": "{date}", "set": "%Y-%m-%d-%Y-%M"}
+        Log file vars should be sent as a list of dict -> [{"var": "{date}", "set": "%Y-%m-%d-%Y-%M"}]
 
         Supported log file vars:
             {date} - will be replaced with the current date using the provided strftime format
@@ -67,7 +66,7 @@ def create_logger(console_level=WARNING, log_file='', file_level=WARNING, name='
     # Console
     if console:
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(_log_level_name.get(console_level.upper(), logging.INFO) if not isinstance(console_level, int) else console_level)
+        console_handler.setLevel(console_level if isinstance(console_level, int) else _log_level_number.get(str(console_level).upper(), DEFAULT_LEVEL))
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
 
@@ -75,18 +74,19 @@ def create_logger(console_level=WARNING, log_file='', file_level=WARNING, name='
     if syslog:
         syslog_formatter = logging.Formatter(syslog_script_name + '[%(process)d]: %(levelname)s: %(message)s')
         syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
-        syslog_handler.setLevel(_log_level_name.get(file_level.upper(), logging.WARNING) if not isinstance(file_level, int) else file_level)
+        syslog_handler.setLevel(file_level if isinstance(file_level, int) else _log_level_number.get(str(file_level).upper(), DEFAULT_LEVEL))
         syslog_handler.setFormatter(syslog_formatter)
         logger.addHandler(syslog_handler)
 
     # file
     if log_file != '':
         # replace variables in the log file name
-        for var in log_file_vars:
-            if type(var) == dict and 'var' in var and 'set' in var and var['var'] == "{date}":
-                log_file = log_file.replace(var['var'], datetime.now().strftime(var['set']))
+        if log_file_vars is not None:
+            for var in log_file_vars:
+                if isinstance(var, dict) and 'var' in var and 'set' in var and var['var'] == "{date}":
+                    log_file = log_file.replace(var['var'], datetime.now().strftime(var['set']))
         file_handler = logging.FileHandler(log_file, mode=file_mode, encoding='utf-8', delay=False)
-        file_handler.setLevel(_log_level_name.get(file_level.upper(), logging.WARNING) if not isinstance(file_level, int) else file_level)
+        file_handler.setLevel(file_level if isinstance(file_level, int) else _log_level_number.get(str(file_level).upper(), DEFAULT_LEVEL))
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
@@ -94,8 +94,9 @@ def create_logger(console_level=WARNING, log_file='', file_level=WARNING, name='
         if log_file_retention_days > 0:
             # replace variables with a *
             log_file_search_name = log_file
-            for var in log_file_vars:
-                log_file_search_name.replace(var['var'], '*')
+            if log_file_vars is not None:
+                for var in log_file_vars:
+                    log_file_search_name.replace(var['var'], '*')
             old_log_files = glob(log_file_search_name)
             for old_log_file in old_log_files:
                 # check the age and delete if needed
